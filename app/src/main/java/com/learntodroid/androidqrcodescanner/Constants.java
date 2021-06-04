78e668633b62d11c38f080d48d40eca4f156d876
Eya Arfa <arfaaya0@gmail.com>
2021-06-04 21:11:39 +0100

    changes

---
+++ app/src/main/java/com/learntodroid/androidqrcodescanner/Constants.java
@@ -0,0 +1,7 @@
+package com.learntodroid.androidqrcodescanner;
+
+import java.util.Date;
+
+public abstract class Constants {
+    public static long IntervalMonth=2592000000L;
+}
---
+++ app/src/main/java/com/learntodroid/androidqrcodescanner/utils/Save.java
@@ -0,0 +1,20 @@
+package com.learntodroid.androidqrcodescanner.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Save {
+
+    public static void save(Context ctx,String name , String value)
+    {
+        SharedPreferences s= ctx.getSharedPreferences("clipcodes",Context.MODE_PRIVATE);
+        SharedPreferences.Editor edt = s.edit();
+        edt.putString(name,value);
+        edt.apply();
+    }
+    public static String read(Context ctx,String name,String value){
+        SharedPreferences s=ctx.getSharedPreferences("clipcodes",Context.MODE_PRIVATE);
+        return s.getString(name,value);
+    }
+
+}
